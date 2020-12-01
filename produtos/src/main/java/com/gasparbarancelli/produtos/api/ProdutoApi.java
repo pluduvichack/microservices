7040707443f495eb771b7e200f1a25f306222a60
pablo <senha1234>
2020-12-01 20:41:25 -0300

    init

--- produtos/src/main/java/com/gasparbarancelli/produtos/api/ProdutoApi.java
+++ produtos/src/main/java/com/gasparbarancelli/produtos/api/ProdutoApi.java
@@ -1,8 +1,11 @@
 package com.gasparbarancelli.produtos.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gasparbarancelli.produtos.model.Produto;
 import com.gasparbarancelli.produtos.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,6 +18,12 @@ public class ProdutoApi {
     @Autowired
     private ProdutoRepository repository;
 
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @GetMapping
     public List<Produto> all() {
         return repository.findAll();
@@ -26,8 +35,13 @@ public class ProdutoApi {
     }
 
     @PostMapping
-    public Produto insert(@RequestBody Produto produto) {
-        return repository.save(produto);
+    public Produto insert(@RequestBody Produto produto) throws JsonProcessingException {
+        repository.save(produto);
+
+        String produtoJson = objectMapper.writeValueAsString(produto);
+        jmsTemplate.convertAndSend("queue.produto.insert", produtoJson);
+
+        return produto;
     }
 
     @DeleteMapping("{id}")
