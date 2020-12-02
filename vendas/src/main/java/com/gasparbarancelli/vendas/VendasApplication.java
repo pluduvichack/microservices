da9dfc210b832adcaa7a2dd501d03a1d330cbd80
pablo <senha1234>
2020-12-02 14:18:32 -0300

    init

--- vendas/src/main/java/com/gasparbarancelli/vendas/VendasApplication.java
+++ vendas/src/main/java/com/gasparbarancelli/vendas/VendasApplication.java
@@ -2,8 +2,10 @@ package com.gasparbarancelli.vendas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class VendasApplication {
 
 	public static void main(String[] args) {
