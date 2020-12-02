428571ace3a481a8a00bc5eccdfeae1b819db8d3
pablo <senha1234>
2020-12-02 15:00:44 -0300

    init

---
+++ cupons/src/main/java/com/gasparbarancelli/cupons/CuponsApplication.java
@@ -0,0 +1,15 @@
+package com.gasparbarancelli.cupons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CuponsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuponsApplication.class, args);
+	}
+
+}
